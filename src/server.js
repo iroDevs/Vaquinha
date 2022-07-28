a3b71cd6ffdd9d2df403433d02dbd2acb9d655e8
PedroDev <pedrohenriquelouresoliveira@gmail.com>
2022-07-28 18:05:48 -0300

    swagger

--- src/server.js
+++ src/server.js
@@ -1,10 +1,15 @@
+
+const swaggerDoc = require('./swagger.json')
+const swaggerUi = require('swagger-ui-express')
+
 const express = require('express')
 require('dotenv').config()
-
 
 const app = express()
 const port = process.env.NODE_PORT || 5000
 
+app.use(express.json())
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc))
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
